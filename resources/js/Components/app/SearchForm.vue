3d716d60a387a8faebe44e6f124cb29f9fa952f3
Rubel <31316317+Rubelsoldier@users.noreply.github.com>
2024-04-08 19:21:30 +0600

    search form, headless ui, heroicons

--- resources/js/Components/app/SearchForm.vue
+++ resources/js/Components/app/SearchForm.vue
@@ -1,30 +1,48 @@
 <template>
-    <div>
-        Search Form
+    <div class="w-[600px] h-[80px] flex items-center">
+        <TextInput type="text"
+                   class="block w-full mr-2"
+                   v-model="search"
+                   autocomplete            
+                   placeholder="Search for files and folders"
+        />
     </div>
 </template>
 
 <script setup>
+// Imports
+import TextInput from "@/Components/TextInput.vue";
+// import {router, useForm} from "@inertiajs/vue3";
+// import {onMounted, ref} from "vue";
+// import {emitter, ON_SEARCH} from "@/event-bus.js";
 
-//Imports
+// Uses
 
+// let params = ''
 
-//Uses
+// Refs
+// const search = ref('')
 
+// Props & Emit
 
-//Refs
+// Computed
 
+// Methods
+// function onSearch() {
+//     params.set('search', search.value)
+//     router.get(window.location.pathname + '?' + params.toString())
 
-//Props and Emit
+//     emitter.emit(ON_SEARCH, search.value)
+// }
 
-
-//Computed
-
-
-//Methods
-
-
-//Hooks
-
+// Hooks
+// onMounted(() => {
+//     params = new URLSearchParams(window.location.search)
+//     search.value = params.get('search')
+// })
 
 </script>
+
+<style scoped>
+
+</style>
